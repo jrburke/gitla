5e7ac1d049330a5e88ddc7a9c8e8c2702e31430f
jrburke <jrburke@gmail.com>
2011-04-18 11:16:59 -0700

    Fleshing out API, but does not work.

--- README.md
+++ README.md
@@ -1,10 +1,56 @@
 
 
-Got to https://github.com/account/applications/new 
 
-Name: gitla: Git/Bugzilla Workflow App 
+Got to https://github.com/account/applications/new
+
+Name: gitla: Git/Bugzilla Workflow App
 URL: http://127.0.0.1:44852/
 Callback URL: http://127.0.0.1:44852/oauthDone
 
 Put the info in a gitla.json file that is in this directory...
 
+
+The examples below use **develop** as the target branch names for bug branch
+merges, but any valid branch name can be used instead.
+
+## Create a bug branch
+
+    gitla.js branch 12345 develop
+
+This will create a branch named bug/12345 from the develop branch.
+
+No Bugzilla or GitHub work is done as part of this command. It just enforces the
+bug/12345 pattern for branch names.
+
+## Ask for a review
+
+    gitla.js r?:jrburke "message" develop
+
+Asks jrburke for a review of the current branch by creating a pull request against **develop**,
+then creating a bug attachment for that pull request in the bug with the same number
+as the current branch (current branch must have the pattern of bug/12345).
+
+The "message" will show up in the GitHub pull request and in the Bugzilla attachment.
+
+## Merge a bug branch
+
+    gitla.js merge "r=jrburke" develop (uses current branch name to merge into develop)
+
+Merges the current branch (which must have pattern bug/12345) into **develop**, using the message "r=jrburke".
+
+This command will do a --no-ff merge with a commit message follows the form:
+
+    "Bug 12345 - Title of Bugzilla bug. " + "message"
+
+Once the merge is complete:
+
+* the resulting merge to "develop" is pushed to "origin"
+* the Bugzilla bug is marked closed/resolved with a "Fixed in https:/github.com/commit/changeset/url".
+
+GitHub will automatically close the pull request, so no work is done as part of this command.
+
+## Delete a bug branch
+
+    gitla.js delete 12345
+
+Deletes the bug/12345 branch. Removes the local and remote branches.
--- gitla.js
+++ gitla.js
@@ -1,5 +1,3 @@
-#!/usr/local/bin/node
-
 /*jslint strict: false */
 /*globals require: false, process: false, console: false */
 
@@ -9,10 +7,120 @@ var fs = require('fs'),
     https = require('https'),
     url = require('url'),
     spawn = require('child_process').spawn,
+    exec = require('child_process').exec,
     myDir = path.dirname(process.argv[1]),
     configPath = path.join(myDir, 'gitla.json'),
     config = {},
-    oauthConfig;
+    oauthConfig, server;
+
+function err(message) {
+    console.log(message);
+    process.exit(1);
+}
+
+function makeBugBranchName(value) {
+    if (!value.matches(/^\d+$/)) {
+        err('Not a valid bug format: ' + value);
+    }
+    return 'bug/' + value;
+}
+
+function validateRefBranch(value) {
+    //Just make sure it has a value.
+    if (!value) {
+        err('Please specify the reference branch the bug branch is ' +
+                    'based on (usually called develop or master).');
+    }
+    return value;
+}
+
+function help() {
+    console.log('Opening web page to show help.');
+    spawn('open', ['https://github.com/jrburke/gitla/blob/master/README.md']);
+}
+
+function branch(args) {
+    var bugBranch = makeBugBranchName(args[0]),
+        refBranch = validateRefBranch(args[1]);
+
+    exec('git checkout ' + refBranch, function (error, stdout, stderr) {
+        if (error) {
+            err('Cannot checkout ' + refBranch + ': ' + error);
+        }
+
+        exec('git checkout -b ' + bugBranch, function (error, stdout, stderr) {
+            if (error) {
+                err('Cannot create branch ' + bugBranch + ': ' + error);
+            }
+        });
+    });
+}
+
+function review(args) {
+    //Create the pull request
+
+    //Update the bugzilla bug with the attachment to pull request.
+}
+
+function merge(args) {
+
+    //Pull the bug branch
+
+    //Switch to develop
+
+    //Merge the bug branch with develop
+
+    //Push the merge
+
+    //Update the bugzilla bug
+
+}
+
+/**
+ * Deletes the local and remote branches for a bug branch.
+ */
+function del(args) {
+    //Delete the local branch
+
+    //Delete the remote branch
+    var bugBranch = makeBugBranchName(args[0]);
+
+    exec('git branch -d ' + bugBranch, function (error, stdout, stderr) {
+        if (error) {
+            err('Cannot delete branch ' + bugBranch + ': ' + error);
+        }
+
+        exec('git push origin :' + bugBranch, function (error, stdout, stderr) {
+            if (error) {
+                err('Cannot delete remote branch ' + bugBranch + ': ' + error);
+            }
+        });
+    });
+
+}
+
+/**
+ * Routes the action to the right function based
+ * on the first argument passed.
+ */
+function route() {
+    var args = process.argv.slice(2),
+        action = args[0],
+        actionArgs = args.splice(1);
+
+    console.log("args: " + args);
+
+    if (action === 'branch') {
+        branch(actionArgs);
+    } else if (action === 'merge') {
+        merge(actionArgs);
+    } else if (action.indexOf('r?') === 0) {
+        actionArgs.push(action.substring(2));
+        review(actionArgs);
+    } else {
+        help();
+    }
+}
 
 //First read the config file.
 if (!path.existsSync(configPath)) {
@@ -26,16 +134,16 @@ config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 ['gitHubOAuth'].forEach(function (prop) {
     if (!config[prop]) {
         console.log('Config file needs property: ' + prop);
-        process.exit(1);
+        spawn('open', ['https://github.com/login/oauth/authorize?client_id=' + oauthConfig.clientId]);
     }
 });
 
 oauthConfig = config.gitHubOAuth;
 
 //If need OAuth credentials, do the jump
-if (!config.oauth) {
+if (!config.accessToken) {
     //Start the web server
-    http.createServer(function (req, res) {
+    (server = http.createServer(function (req, res) {
         var parsed = url.parse(req.url, true),
             code, tokenUrlParts, options, httpsReq;
 
@@ -69,14 +177,16 @@ if (!config.oauth) {
                     var parsedResponse = url.parse('/bogus.html?' + postResponse.trim(), true);
                     config.accessToken = parsedResponse.query.access_token;
                     fs.writeFileSync(configPath, JSON.stringify(config, null, '  '), 'utf8');
-                    process.exit(0);
+                    server.close();
+                    route();
                 });
 
                 httpsRes.on('error', function (error) {
                     console.log('Request to github failed:');
                     console.log(error);
+                    server.close();
                     process.exit(1);
-                })
+                });
             });
             httpsReq.end();
 
@@ -87,11 +197,13 @@ if (!config.oauth) {
         }
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('All Done. You can close this window now.\n');
-    }).listen(44852, "127.0.0.1");
+    })).listen(44852, "127.0.0.1");
 
     console.log('Waiting for OAuth dance to finish...');
 
     //Open the browser
     spawn('open', ['https://github.com/login/oauth/authorize?client_id=' + oauthConfig.clientId]);
 
+} else {
+    route();
 }
